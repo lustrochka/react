caa7e3bfa8290dee412d630277e557e6cf2b32be
Olga <kartoshkaslukom@gmail.com>
2023-10-27 13:25:56 +0200

    feat: add error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,87 +1,14 @@
 import { Component } from 'react';
-import axios from 'axios';
-import imgUrl from './assets/PIWO.svg';
+import { ErrorBoundary } from './modules/ErrorBoundary';
+import { Page } from './modules/Page';
 import './App.scss';
 
-interface responseItem {
-  id: number;
-  name: string;
-  description: string;
-}
-
 class App extends Component {
-  state = {
-    beers: [],
-    searchString: localStorage.getItem('searchString') || '',
-    loadingClass: 'loading',
-    resClass: 'results hiding',
-  };
-  componentDidMount(): void {
-    this.search();
-  }
-  search() {
-    const BASE_URL = 'https://api.punkapi.com/v2/beers/';
-    const url = this.state.searchString
-      ? `${BASE_URL}?beer_name=${this.state.searchString.replace(' ', '_')}`
-      : BASE_URL;
-    axios.get(url).then((res) => {
-      this.setState({
-        loadingClass: 'loading hiding',
-        resClass: 'results',
-        beers: res.data,
-      });
-    });
-  }
   render() {
     return (
-      <>
-        <div className="search">
-          <input
-            type="search"
-            value={this.state.searchString}
-            onChange={(event) =>
-              this.setState({ searchString: event.target.value.trim() })
-            }
-          ></input>
-          <button
-            onClick={() => {
-              this.setState({
-                loadingClass: 'loading',
-                resClass: 'results hiding',
-              });
-              localStorage.setItem('searchString', this.state.searchString);
-              this.search();
-            }}
-          >
-            🔍
-          </button>
-        </div>
-        <div className={this.state.loadingClass}>
-          <div>
-            <img src={imgUrl} className="bubble"></img>
-          </div>
-          <div>
-            <img src={imgUrl} className="bubble"></img>
-          </div>
-          <div>
-            <img src={imgUrl} className="bubble"></img>
-          </div>
-          <div>
-            <img src={imgUrl} className="bubble"></img>
-          </div>
-          <div>
-            <img src={imgUrl} className="bubble"></img>
-          </div>
-        </div>
-        <div className={this.state.resClass}>
-          {this.state.beers.map((item: responseItem) => (
-            <div key={item.id.toString()}>
-              <h2>{item.name}</h2>
-              <div>{item.description}</div>
-            </div>
-          ))}
-        </div>
-      </>
+      <ErrorBoundary>
+        <Page />
+      </ErrorBoundary>
     );
   }
 }
---
+++ src/modules/Page.tsx
@@ -0,0 +1,99 @@
+import { Component } from 'react';
+import axios from 'axios';
+import imgUrl from '../assets/PIWO.svg';
+import '../App.scss';
+
+interface responseItem {
+  id: number;
+  name: string;
+  description: string;
+  error: boolean;
+}
+
+export class Page extends Component {
+  state = {
+    beers: [],
+    searchString: localStorage.getItem('searchString') || '',
+    loadingClass: 'loading',
+    resClass: 'results hiding',
+    error: false,
+  };
+  componentDidMount(): void {
+    this.search();
+  }
+  componentDidUpdate() {
+    if (this.state.error) throw new Error('This is an error');
+  }
+  search() {
+    const BASE_URL = 'https://api.punkapi.com/v2/beers/';
+    const url = this.state.searchString
+      ? `${BASE_URL}?beer_name=${this.state.searchString.replace(' ', '_')}`
+      : BASE_URL;
+    axios.get(url).then((res) => {
+      this.setState({
+        loadingClass: 'loading hiding',
+        resClass: 'results',
+        beers: res.data,
+      });
+    });
+  }
+  render() {
+    return (
+      <>
+        <button
+          onClick={() => {
+            this.setState({ error: true });
+          }}
+        >
+          Make an error
+        </button>
+        <div className="search">
+          <input
+            type="search"
+            value={this.state.searchString}
+            onChange={(event) =>
+              this.setState({ searchString: event.target.value.trim() })
+            }
+          ></input>
+          <button
+            onClick={() => {
+              this.setState({
+                loadingClass: 'loading',
+                resClass: 'results hiding',
+              });
+              localStorage.setItem('searchString', this.state.searchString);
+              this.search();
+            }}
+          >
+            🔍
+          </button>
+        </div>
+        <div className={this.state.loadingClass}>
+          <div>
+            <img src={imgUrl} className="bubble"></img>
+          </div>
+          <div>
+            <img src={imgUrl} className="bubble"></img>
+          </div>
+          <div>
+            <img src={imgUrl} className="bubble"></img>
+          </div>
+          <div>
+            <img src={imgUrl} className="bubble"></img>
+          </div>
+          <div>
+            <img src={imgUrl} className="bubble"></img>
+          </div>
+        </div>
+        <div className={this.state.resClass}>
+          {this.state.beers.map((item: responseItem) => (
+            <div key={item.id.toString()}>
+              <h2>{item.name}</h2>
+              <div>{item.description}</div>
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  }
+}
